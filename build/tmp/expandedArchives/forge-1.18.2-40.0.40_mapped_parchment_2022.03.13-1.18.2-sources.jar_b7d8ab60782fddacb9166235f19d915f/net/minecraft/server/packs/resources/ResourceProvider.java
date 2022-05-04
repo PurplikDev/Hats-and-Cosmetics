ca4315420f788fdc1f723a5634da0562f3cd2680
Purplik <60230933+PurplikDev@users.noreply.github.com>
2022-05-04 15:05:27 +0200

    Buildus Deleten

--- build/tmp/expandedArchives/forge-1.18.2-40.0.40_mapped_parchment_2022.03.13-1.18.2-sources.jar_b7d8ab60782fddacb9166235f19d915f/net/minecraft/server/packs/resources/ResourceProvider.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.server.packs.resources;
-
-import java.io.IOException;
-import net.minecraft.resources.ResourceLocation;
-
-@FunctionalInterface
-public interface ResourceProvider {
-   Resource getResource(ResourceLocation pResourceLocation) throws IOException;
-}
